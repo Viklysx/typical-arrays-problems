31ab414a122b3dc8c72ebc15dd81da47aaa9d56b
Viklysx <vikbelec111@yandex.ru>
2020-10-01 01:59:20 +0300

    feat: added function handling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array) {
+    if (array.length !== 0) {
+      let minVFalue = array[0];
+      for (i = 1; i < array.length; ++i) {
+        if (array[i] < minVFalue) minVFalue = array[i];
+      }
+      return minVFalue
+    }
+    else return 0;
+  }
+  else return 0;
 }
 
-exports.max = function max (array) {
+exports.max = function max (array) { 
+  if (array) {
+    if (array.length !== 0) {
+      let maxVFalue = array[0];
+      for (i = 1; i < array.length; ++i) {
+        if (array[i] > maxVFalue) maxVFalue = array[i];
+      }
+      return maxVFalue
+    }
+    else return 0;
+  }
+  else return 0;
   return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  if (array){
+    if (array.length !== 0) {
+      array.forEach(item => {
+        sum += item;
+      });
+      return sum/array.length
+    }
+    else return 0;
+  }
+  else return 0;
 }
